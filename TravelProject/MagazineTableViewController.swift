72f28deffe3130b28834c1070286f63bde665d48
Woohyun Jo <bluewing1004@gmail.com>
2025-01-03 20:54:09 +0900

    [Add] Magazine, Travel 더미데이터 세팅 및 구조체 생성

---
+++ TravelProject/MagazineTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MagazineTableViewController.swift
+//  TravelProject
+//
+//  Created by 조우현 on 1/3/25.
+//
+
+import UIKit
+
+class MagazineTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
