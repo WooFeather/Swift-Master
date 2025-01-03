6d30ef16402dfd22e30e7dbef335b62729035016
Woohyun Jo <bluewing1004@gmail.com>
2025-01-03 20:05:16 +0900

    Initial Commit

---
+++ Assignment8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment8
+//
+//  Created by 조우현 on 1/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
