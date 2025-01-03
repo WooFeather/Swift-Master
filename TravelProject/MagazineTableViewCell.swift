f2b8d3e91f867090c2e136f464014928b0a827b8
Woohyun Jo <bluewing1004@gmail.com>
2025-01-03 21:09:23 +0900

    [Setting] MagazineViewCell 로직파일 생성 및 MagazineTVC 로직파일 기본세팅

---
+++ TravelProject/MagazineTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MagazineTableViewCell.swift
+//  TravelProject
+//
+//  Created by 조우현 on 1/3/25.
+//
+
+import UIKit
+
+class MagazineTableViewCell: UITableViewCell {
+
+}
--- TravelProject/MagazineTableViewController.swift
+++ TravelProject/MagazineTableViewController.swift
@@ -9,8 +9,25 @@ import UIKit
 
 class MagazineTableViewController: UITableViewController {
 
+    var magazines = MagazineInfo().magazine
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        magazines.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MagazineTableViewCell", for: indexPath)
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 480
     }
 }
