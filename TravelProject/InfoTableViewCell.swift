69b9a9c47f44304633694487069d218217f428ba
Woohyun Jo <bluewing1004@gmail.com>
2025-01-04 00:44:16 +0900

    [Setting] InfoTableViewCell 로직파일 생성 및 InfoTVC 로직파일 기본세팅

---
+++ TravelProject/InfoTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  InfoTableViewCell.swift
+//  TravelProject
+//
+//  Created by 조우현 on 1/4/25.
+//
+
+import UIKit
+
+class InfoTableViewCell: UITableViewCell {
+    
+}
--- TravelProject/InfoTableViewController.swift
+++ TravelProject/InfoTableViewController.swift
@@ -8,9 +8,27 @@
 import UIKit
 
 class InfoTableViewController: UITableViewController {
+    
+    let travels = TravelInfo().travel
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        travels.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "InfoTableViewCell", for: indexPath) as! InfoTableViewCell
+        
+    
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 164
     }
 }
