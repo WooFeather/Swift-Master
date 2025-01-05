2c22a13851f70b1d53362cbd7abb75778e91fab4
Woohyun Jo <bluewing1004@gmail.com>
2025-01-05 21:26:44 +0900

    [Add] Checklist 구조체 생성

---
+++ TravelProject/Checklist/Checklist.swift
@@ -0,0 +1,14 @@
+//
+//  Checklist.swift
+//  TravelProject
+//
+//  Created by 조우현 on 1/5/25.
+//
+
+import Foundation
+
+struct Checklist {
+    var title: String?
+    var bookmark: Bool?
+    var complete: Bool?
+}
