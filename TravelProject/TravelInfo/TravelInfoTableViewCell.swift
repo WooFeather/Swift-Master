712790a4b332a11cb3e0c0e04ac0be6e824a076b
Woohyun Jo <bluewing1004@gmail.com>
2025-01-06 19:45:56 +0900

    [Fix] TravelInfoTVC CellForRowAt -> Cell파일로 코드정리

--- TravelProject/TravelInfo/TravelInfoTableViewCell.swift
+++ TravelProject/TravelInfo/TravelInfoTableViewCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 class TravelInfoTableViewCell: UITableViewCell {
     @IBOutlet var titleLabel: UILabel!
@@ -14,4 +15,43 @@ class TravelInfoTableViewCell: UITableViewCell {
     @IBOutlet var saveLabel: UILabel!
     @IBOutlet var travelImageView: UIImageView!
     @IBOutlet var likeButton: UIButton!
+    
+    func configureData(row: Travel) {
+        titleLabel.text = row.title
+        titleLabel.font = .systemFont(ofSize: 18, weight: .bold)
+        titleLabel.textColor = .magazineTitle
+        
+        descriptionLabel.text = row.description != nil ? row.description : "불러오기 실패"
+        descriptionLabel.font = .systemFont(ofSize: 14, weight: .semibold)
+        descriptionLabel.textColor = .gray
+        descriptionLabel.numberOfLines = 0
+        
+        gradeLabel.text = row.grade != nil ? "\(row.grade!) • " : "불러오기 실패 • "
+        gradeLabel.font = .systemFont(ofSize: 14, weight: .medium)
+        gradeLabel.textColor = .lightGray
+        
+        saveLabel.text = row.save != nil ? "저장 \(row.save!.numberFormatting()!)" : "불러오기 실패"
+        saveLabel.font = .systemFont(ofSize: 14, weight: .medium)
+        saveLabel.textColor = .lightGray
+        
+        if let image = row.travel_image {
+            let url = URL(string: image)
+            travelImageView.kf.setImage(with: url)
+            travelImageView.contentMode = .scaleAspectFill
+            travelImageView.layer.cornerRadius = 10
+        } else {
+            travelImageView.image = UIImage(systemName: "xmark.circle")
+            travelImageView.tintColor = .gray
+        }
+        
+        if let button = row.like {
+            let name = button ? "heart.fill" : "heart"
+            let btnImage = UIImage(systemName: name)
+            
+            likeButton.setImage(btnImage, for: .normal)
+        } else {
+            likeButton.setImage(UIImage(systemName: "questionmark"), for: .normal)
+            likeButton.isUserInteractionEnabled = false
+        }
+    }
 }
--- TravelProject/TravelInfo/TravelInfoTableViewController.swift
+++ TravelProject/TravelInfo/TravelInfoTableViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import Kingfisher
 
 class InfoTableViewController: UITableViewController {
     
@@ -28,49 +27,14 @@ class InfoTableViewController: UITableViewController {
         return travels.count
     }
     
-    // 반복되는 코드 정리해보자
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if travels[indexPath.row].ad == false {
             let cell = tableView.dequeueReusableCell(withIdentifier: "TravelInfoTableViewCell", for: indexPath) as! TravelInfoTableViewCell
             let row = travels[indexPath.row]
             
-            cell.titleLabel.text = row.title
-            cell.titleLabel.font = .systemFont(ofSize: 18, weight: .bold)
-            cell.titleLabel.textColor = .magazineTitle
-            
-            cell.descriptionLabel.text = row.description != nil ? row.description : "불러오기 실패"
-            cell.descriptionLabel.font = .systemFont(ofSize: 14, weight: .semibold)
-            cell.descriptionLabel.textColor = .gray
-            cell.descriptionLabel.numberOfLines = 0
-            
-            cell.gradeLabel.text = row.grade != nil ? "\(row.grade!) • " : "불러오기 실패 • "
-            cell.gradeLabel.font = .systemFont(ofSize: 14, weight: .medium)
-            cell.gradeLabel.textColor = .lightGray
-            
-            cell.saveLabel.text = row.save != nil ? "저장 \(row.save!.numberFormatting()!)" : "불러오기 실패"
-            cell.saveLabel.font = .systemFont(ofSize: 14, weight: .medium)
-            cell.saveLabel.textColor = .lightGray
-            
-            if let image = row.travel_image {
-                let url = URL(string: image)
-                cell.travelImageView.kf.setImage(with: url)
-                cell.travelImageView.contentMode = .scaleAspectFill
-                cell.travelImageView.layer.cornerRadius = 10
-            } else {
-                cell.travelImageView.image = UIImage(systemName: "xmark.circle")
-                cell.travelImageView.tintColor = .gray
-            }
-            
-            if let button = row.like {
-                let name = button ? "heart.fill" : "heart"
-                let btnImage = UIImage(systemName: name)
-                
-                cell.likeButton.setImage(btnImage, for: .normal)
-                cell.likeButton.tag = indexPath.row
-                cell.likeButton.addTarget(self, action: #selector(likeButtonTapped), for: .touchUpInside)
-            } else {
-                cell.likeButton.setImage(UIImage(systemName: "questionmark"), for: .normal)
-            }
+            cell.configureData(row: row)
+            cell.likeButton.tag = indexPath.row
+            cell.likeButton.addTarget(self, action: #selector(likeButtonTapped), for: .touchUpInside)
             
             return cell
         } else {
