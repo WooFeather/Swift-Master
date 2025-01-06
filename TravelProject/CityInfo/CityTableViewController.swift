443aeafe3146eb0eac97786b2ae4b36f0c8ab29e
Woohyun Jo <bluewing1004@gmail.com>
2025-01-06 21:40:16 +0900

    [Add] CityTVC생성 및 로직 파일 생성

---
+++ TravelProject/CityInfo/CityTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CityTableViewController.swift
+//  TravelProject
+//
+//  Created by 조우현 on 1/6/25.
+//
+
+import UIKit
+
+class CityTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
