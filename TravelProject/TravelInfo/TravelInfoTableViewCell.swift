05f401a4fb668bd12dd72b0e2204072b4f011726
Woohyun Jo <bluewing1004@gmail.com>
2025-01-06 20:14:51 +0900

    [Fix] TravelInfoCell 고정 UI -> configure() 함수로 분리

--- TravelProject/TravelInfo/TravelInfoTableViewCell.swift
+++ TravelProject/TravelInfo/TravelInfoTableViewCell.swift
@@ -16,38 +16,42 @@ class TravelInfoTableViewCell: UITableViewCell {
     @IBOutlet var travelImageView: UIImageView!
     @IBOutlet var likeButton: UIButton!
     
-    func configureData(row: Travel) {
-        titleLabel.text = row.title
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        configure()
+    }
+    
+    private func configure() {
         titleLabel.font = .systemFont(ofSize: 18, weight: .bold)
         titleLabel.textColor = .magazineTitle
-        
-        descriptionLabel.text = row.description != nil ? row.description : "불러오기 실패"
         descriptionLabel.font = .systemFont(ofSize: 14, weight: .semibold)
         descriptionLabel.textColor = .gray
         descriptionLabel.numberOfLines = 0
-        
-        gradeLabel.text = row.grade != nil ? "\(row.grade!) • " : "불러오기 실패 • "
         gradeLabel.font = .systemFont(ofSize: 14, weight: .medium)
         gradeLabel.textColor = .lightGray
-        
-        saveLabel.text = row.save != nil ? "저장 \(row.save!.numberFormatting()!)" : "불러오기 실패"
         saveLabel.font = .systemFont(ofSize: 14, weight: .medium)
         saveLabel.textColor = .lightGray
+        travelImageView.contentMode = .scaleAspectFill
+        travelImageView.layer.cornerRadius = 10
+        travelImageView.tintColor = .gray
+    }
+    
+    func configureData(row: Travel) {
+        titleLabel.text = row.title
+        descriptionLabel.text = row.description != nil ? row.description : "불러오기 실패"
+        gradeLabel.text = row.grade != nil ? "\(row.grade!) • " : "불러오기 실패 • "
+        saveLabel.text = row.save != nil ? "저장 \(row.save!.numberFormatting()!)" : "불러오기 실패"
         
         if let image = row.travel_image {
             let url = URL(string: image)
             travelImageView.kf.setImage(with: url)
-            travelImageView.contentMode = .scaleAspectFill
-            travelImageView.layer.cornerRadius = 10
         } else {
             travelImageView.image = UIImage(systemName: "xmark.circle")
-            travelImageView.tintColor = .gray
         }
         
         if let button = row.like {
             let name = button ? "heart.fill" : "heart"
             let btnImage = UIImage(systemName: name)
-            
             likeButton.setImage(btnImage, for: .normal)
         } else {
             likeButton.setImage(UIImage(systemName: "questionmark"), for: .normal)
