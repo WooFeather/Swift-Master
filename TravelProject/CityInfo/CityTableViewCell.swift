bac00a3f2d8d878418b375905d3e119ef1404ad4
Woohyun Jo <bluewing1004@gmail.com>
2025-01-07 00:39:18 +0900

    [Design] XIB cell explain까지 UI작업완료

--- TravelProject/CityInfo/CityTableViewCell.swift
+++ TravelProject/CityInfo/CityTableViewCell.swift
@@ -20,16 +20,20 @@ class CityTableViewCell: UITableViewCell {
     }
     
     private func configure() {
+        // 일부만 cornerRadius주기
         cityImageView.layer.cornerRadius = 20
         cityImageView.contentMode = .scaleAspectFill
         explainBackground.layer.cornerRadius = 20
         cityNameLabel.font = .systemFont(ofSize: 20, weight: .heavy)
         cityNameLabel.textColor = .white
+        cityExplainLabel.textColor = .white
+        cityExplainLabel.font = .systemFont(ofSize: 16, weight: .medium)
     }
     
     func configureData(row: City) {
         let url = URL(string: row.city_image)
         cityImageView.kf.setImage(with: url)
         cityNameLabel.text = row.title
+        cityExplainLabel.text = row.city_explain
     }
 }
