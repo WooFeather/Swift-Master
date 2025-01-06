505ab9b55190af0fbd343a6701f063d1099c091d
Woohyun Jo <bluewing1004@gmail.com>
2025-01-06 19:17:24 +0900

    [Fix] MagazineTVC의 CellForRowAt 메서드 단순화

--- TravelProject/Magazine/MagazineTableViewCell.swift
+++ TravelProject/Magazine/MagazineTableViewCell.swift
@@ -6,10 +6,49 @@
 //
 
 import UIKit
+import Kingfisher
 
 class MagazineTableViewCell: UITableViewCell {
     @IBOutlet var magazineImageView: UIImageView!
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var subtitleLabel: UILabel!
     @IBOutlet var dateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        print(#function)
+        
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        print(#function)
+        
+    }
+    
+    private func configure() {
+        
+    }
+    
+    func configureData(row: Magazine) {
+        print(#function)
+        
+        let url = URL(string: row.photo_image)
+        magazineImageView.kf.setImage(with: url)
+        magazineImageView.contentMode = .scaleAspectFill
+        magazineImageView.layer.cornerRadius = 10
+        
+        titleLabel.text = row.title
+        titleLabel.textColor = .magazineTitle
+        titleLabel.font = .systemFont(ofSize: 20, weight: .bold)
+        titleLabel.numberOfLines = 0
+        
+        subtitleLabel.text = row.subtitle
+        subtitleLabel.font = .systemFont(ofSize: 16, weight: .semibold)
+        subtitleLabel.textColor = .gray
+        
+        dateLabel.text = row.date.toDate()?.toString() ?? "날짜오류"
+        dateLabel.font = .systemFont(ofSize: 14, weight: .semibold)
+        dateLabel.textColor = .gray
+    }
 }
--- TravelProject/Magazine/MagazineTableViewController.swift
+++ TravelProject/Magazine/MagazineTableViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import Kingfisher
 
 class MagazineTableViewController: UITableViewController {
 
@@ -26,24 +25,7 @@ class MagazineTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MagazineTableViewCell", for: indexPath) as! MagazineTableViewCell
         let row = magazines[indexPath.row]
         
-        // 아래 이미지, 타이틀 등을 묶어서 분리할 수 없을까?
-        let url = URL(string: row.photo_image)
-        cell.magazineImageView.kf.setImage(with: url)
-        cell.magazineImageView.contentMode = .scaleAspectFill
-        cell.magazineImageView.layer.cornerRadius = 10
-        
-        cell.titleLabel.text = row.title
-        cell.titleLabel.textColor = .magazineTitle
-        cell.titleLabel.font = .systemFont(ofSize: 20, weight: .bold)
-        cell.titleLabel.numberOfLines = 0
-        
-        cell.subtitleLabel.text = row.subtitle
-        cell.subtitleLabel.font = .systemFont(ofSize: 16, weight: .semibold)
-        cell.subtitleLabel.textColor = .gray
-        
-        cell.dateLabel.text = row.date.toDate()?.toString() ?? "날짜오류"
-        cell.dateLabel.font = .systemFont(ofSize: 14, weight: .semibold)
-        cell.dateLabel.textColor = .gray
+        cell.configureData(row: row)
         
         return cell
     }
