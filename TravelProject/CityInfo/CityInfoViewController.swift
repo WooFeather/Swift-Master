9f039baeaacdb527bc758e8e5b30bb177acb1a96
Woohyun Jo <bluewing1004@gmail.com>
2025-01-07 18:48:59 +0900

    [Design] CityInfoVC 레이아웃 설정

--- TravelProject/CityInfo/CityInfoViewController.swift
+++ TravelProject/CityInfo/CityInfoViewController.swift
@@ -12,18 +12,11 @@ class CityInfoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        navigationDesign()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func navigationDesign() {
+        navigationItem.title = "인기 도시"
     }
-    */
 
 }
