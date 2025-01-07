b64fd21f4ae595fdfe40d0a845b823f44507e027
Woohyun Jo <bluewing1004@gmail.com>
2025-01-07 22:53:30 +0900

    [Feat] 검색 및 필터기능 구현 완료

--- TravelProject/CityInfo/CityInfoViewController.swift
+++ TravelProject/CityInfo/CityInfoViewController.swift
@@ -31,23 +31,105 @@ class CityInfoViewController: UIViewController, UICollectionViewDelegate, UIColl
     }
     
     @IBAction func searchTextFieldDidEndOnExit(_ sender: UITextField) {
+        searchCities()
     }
     
     @IBAction func searchTextFieldEditingChanged(_ sender: UITextField) {
+        searchCities()
     }
     
     @IBAction func citySegmentControlValueChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            print("모두")
+            filteredCities = cities
+            print(filteredCities)
+        case 1:
+            print("국내")
+            filteredCities = cities.filter { $0.domestic_travel }
+            print(filteredCities)
+        case 2:
+            print("해외")
+            filteredCities = cities.filter { $0.domestic_travel == false }
+            print(filteredCities)
+        default:
+            break
+        }
+        cityInfoCollectionView.reloadData()
+    }
+    
+    func searchCities() {
+        let searchText = searchTextField.text!.trimmingCharacters(in: .whitespaces)
+        
+        print(searchText)
+        
+        if searchText.isEmpty {
+            isSearching = false
+            switch citySegmentControl.selectedSegmentIndex {
+            case 0:
+                searchedCities = cities
+            case 1:
+                searchedCities = filteredCities
+            case 2:
+                searchedCities = filteredCities
+            default:
+                searchedCities = cities
+            }
+        } else {
+            isSearching = true
+            switch citySegmentControl.selectedSegmentIndex {
+            case 0:
+                searchedCities = cities.filter {
+                    $0.city_name.contains(searchText) || $0.city_english_name.lowercased().contains(searchText.lowercased()) || $0.city_explain.contains(searchText)
+                }
+            case 1:
+                searchedCities = filteredCities.filter {
+                    $0.city_name.contains(searchText) || $0.city_english_name.lowercased().contains(searchText.lowercased()) || $0.city_explain.contains(searchText)
+                }
+            case 2:
+                searchedCities = filteredCities.filter {
+                    $0.city_name.contains(searchText) || $0.city_english_name.lowercased().contains(searchText.lowercased()) || $0.city_explain.contains(searchText)
+                }
+            default:
+                searchedCities = cities.filter {
+                    $0.city_name.contains(searchText) || $0.city_english_name.lowercased().contains(searchText.lowercased()) || $0.city_explain.contains(searchText)
+                }
+            }
+        }
+        print(searchedCities)
+        cityInfoCollectionView.reloadData()
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return cities.count
+        switch citySegmentControl.selectedSegmentIndex {
+        case 0:
+            return isSearching ? searchedCities.count : cities.count
+        case 1:
+            return isSearching ? searchedCities.count : filteredCities.count
+        case 2:
+            return isSearching ? searchedCities.count : filteredCities.count
+        default:
+            return isSearching ? searchedCities.count : cities.count
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = cityInfoCollectionView.dequeueReusableCell(withReuseIdentifier: Identifier.cityInfoCollectionViewCell.rawValue, for: indexPath) as! CityInfoCollectionViewCell
-        let item = cities[indexPath.item]
         
-        cell.configureData(item: item)
+        switch citySegmentControl.selectedSegmentIndex {
+        case 0:
+            let item = isSearching ? searchedCities[indexPath.item] : cities[indexPath.item]
+            cell.configureData(item: item)
+        case 1:
+            let item = isSearching ? searchedCities[indexPath.item] : filteredCities[indexPath.item]
+            cell.configureData(item: item)
+        case 2:
+            let item = isSearching ? searchedCities[indexPath.item] : filteredCities[indexPath.item]
+            cell.configureData(item: item)
+        default:
+            let item = isSearching ? searchedCities[indexPath.item] : cities[indexPath.item]
+            cell.configureData(item: item)
+        }
         
         return cell
     }
