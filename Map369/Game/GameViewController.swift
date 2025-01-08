77688231363afbeb4bcd89113c89e054a1a3bddb
Woohyun Jo <bluewing1004@gmail.com>
2025-01-08 16:41:02 +0900

    [Setting] 각 VC 세팅 및 로직파일 생성

---
+++ Map369/Game/GameViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GameViewController.swift
+//  Map369
+//
+//  Created by 조우현 on 1/8/25.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ Map369/Map/MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  Map369
+//
+//  Created by 조우현 on 1/8/25.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
