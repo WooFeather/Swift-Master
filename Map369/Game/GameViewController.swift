9a0294e510de53d7c748aa019ec1ba2754a622a1
Woohyun Jo <bluewing1004@gmail.com>
2025-01-08 21:07:32 +0900

    [Design] GameVC 디자인

--- Map369/Game/GameViewController.swift
+++ Map369/Game/GameViewController.swift
@@ -9,9 +9,41 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var selectTextField: UITextField!
+    @IBOutlet var resultTextView: UITextView!
+    @IBOutlet var countLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        titleLabelConfig()
+        selectTextFieldConfig()
+        resultTextViewConfig()
+        countLabelConfig()
     }
 
+    func titleLabelConfig() {
+        titleLabel.text = "369 게임"
+        titleLabel.font = .systemFont(ofSize: 30, weight: .bold)
+    }
+    
+    func selectTextFieldConfig() {
+        selectTextField.placeholder = "최대 숫자를 입력해주세요"
+        selectTextField.font = .systemFont(ofSize: 20)
+        selectTextField.textAlignment = .center
+        selectTextField.borderStyle = .line
+    }
+    
+    func resultTextViewConfig() {
+        resultTextView.isUserInteractionEnabled = false
+        resultTextView.font = .systemFont(ofSize: 20)
+        resultTextView.textColor = .lightGray
+    }
+    
+    func countLabelConfig() {
+        countLabel.font = .systemFont(ofSize: 30, weight: .bold)
+        countLabel.textAlignment = .center
+        countLabel.numberOfLines = 0
+    }
 }
