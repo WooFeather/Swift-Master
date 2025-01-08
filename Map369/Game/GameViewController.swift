eac01bed29f6d57aa9540fcc693cf7773ca3a2e2
Woohyun Jo <bluewing1004@gmail.com>
2025-01-08 21:16:53 +0900

    [Feat] TextField탭했을 때 Picker띄우기

--- Map369/Game/GameViewController.swift
+++ Map369/Game/GameViewController.swift
@@ -7,41 +7,68 @@
 
 import UIKit
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var selectTextField: UITextField!
     @IBOutlet var resultTextView: UITextView!
     @IBOutlet var countLabel: UILabel!
     
+    let pickerView = UIPickerView()
+    var pickerItems: [Int] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        titleLabelConfig()
+        pickerItemsDesign()
         selectTextFieldConfig()
-        resultTextViewConfig()
-        countLabelConfig()
+        titleLabelDesign()
+        selectTextFieldConfig()
+        resultTextViewDesign()
+        countLabelDesign()
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        pickerItems.count
+    }
+    
+    func selectTextFieldConfig() {
+        selectTextField.tintColor = .clear
+        selectTextField.inputView = pickerView
+        
+        pickerView.delegate = self
+        pickerView.dataSource = self
+    }
+    
+    func pickerItemsDesign() {
+        for i in 0...100 {
+            pickerItems.append(i)
+        }
     }
 
-    func titleLabelConfig() {
+    func titleLabelDesign() {
         titleLabel.text = "369 게임"
         titleLabel.font = .systemFont(ofSize: 30, weight: .bold)
     }
     
-    func selectTextFieldConfig() {
+    func selectTextFieldDesign() {
         selectTextField.placeholder = "최대 숫자를 입력해주세요"
         selectTextField.font = .systemFont(ofSize: 20)
         selectTextField.textAlignment = .center
         selectTextField.borderStyle = .line
     }
     
-    func resultTextViewConfig() {
+    func resultTextViewDesign() {
         resultTextView.isUserInteractionEnabled = false
         resultTextView.font = .systemFont(ofSize: 20)
         resultTextView.textColor = .lightGray
     }
     
-    func countLabelConfig() {
+    func countLabelDesign() {
         countLabel.font = .systemFont(ofSize: 30, weight: .bold)
         countLabel.textAlignment = .center
         countLabel.numberOfLines = 0
