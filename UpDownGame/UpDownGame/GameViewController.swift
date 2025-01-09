02b422e5b0a00238a5a44349bf17f3c88ed6db46
Woohyun Jo <bluewing1004@gmail.com>
2025-01-09 19:12:03 +0900

    [Design] GameVC CollectionView를 제외한 아웃렛 작업

--- UpDownGame/UpDownGame/GameViewController.swift
+++ UpDownGame/UpDownGame/GameViewController.swift
@@ -15,10 +15,37 @@ class GameViewController: UIViewController {
     @IBOutlet var resultButton: UIButton!
     
     var selectedNumber: Int?
+    var guideText: String = "UP DOWN"
+    var tryCount: Int = 0
+    var tryCountText: String {
+        "시도 횟수: \(tryCount)"
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         print("선택한 숫자: \(selectedNumber!)")
+        
+        view.backgroundColor = .main
+        guideLabelDesign()
+        tryCountLabelDesign()
+        resultButtonDesign()
+    }
+    
+    func guideLabelDesign() {
+        guideLabel.text = guideText
+        guideLabel.font = .systemFont(ofSize: 50, weight: .heavy)
+        guideLabel.textAlignment = .center
+    }
+    
+    func tryCountLabelDesign() {
+        tryCountLabel.text = tryCountText
+        tryCountLabel.font = .systemFont(ofSize: 20, weight: .regular)
+        tryCountLabel.textAlignment = .center
+    }
+    
+    func resultButtonDesign() {
+        resultButton.setTitle("결과 확인하기", for: .normal)
+        resultButton.tintColor = .white
+        resultButton.backgroundColor = .black
     }
 }
