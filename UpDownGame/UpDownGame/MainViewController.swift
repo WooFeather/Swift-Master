399a9b841eb7196d4345bc10b8adda0e899e968c
Woohyun Jo <bluewing1004@gmail.com>
2025-01-09 17:32:22 +0900

    [Design] MainView 레이아웃작업 및 Outlet연결

---
+++ UpDownGame/UpDownGame/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UpDownGame
+//
+//  Created by 조우현 on 1/9/25.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var subTitleLabel: UILabel!
+    @IBOutlet var mainImageView: UIImageView!
+    @IBOutlet var numberTextField: UITextField!
+    @IBOutlet var startButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
