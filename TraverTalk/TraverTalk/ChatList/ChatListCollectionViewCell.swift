4fb6428266f4504c8d601ee09c16a5038e361d7a
Woohyun Jo <bluewing1004@gmail.com>
2025-01-10 15:34:22 +0900

    [13th/Design] ChatListCell 레이아웃
    Label사이 EqualHeight 적용

---
+++ TraverTalk/TraverTalk/ChatList/ChatListCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ChatListCollectionViewCell.swift
+//  TraverTalk
+//
+//  Created by 조우현 on 1/10/25.
+//
+
+import UIKit
+
+class ChatListCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
