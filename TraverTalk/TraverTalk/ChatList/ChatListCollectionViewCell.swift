4f4936b3734a7df74a4fe39760eab7eee0250c14
Woohyun Jo <bluewing1004@gmail.com>
2025-01-10 16:22:23 +0900

    [13th/Design] ChatListVC 아웃렛 연결 및XIB 기본세팅

--- TraverTalk/TraverTalk/ChatList/ChatListCollectionViewCell.swift
+++ TraverTalk/TraverTalk/ChatList/ChatListCollectionViewCell.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class ChatListCollectionViewCell: UICollectionViewCell {
-
+    @IBOutlet var chatRoomImage: UIImageView!
+    @IBOutlet var chatRoomTitleLabel: UILabel!
+    @IBOutlet var chatRoomPreviewLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
+    
 }
--- TraverTalk/TraverTalk/ChatList/ChatListViewController.swift
+++ TraverTalk/TraverTalk/ChatList/ChatListViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ChatListViewController: UIViewController {
-
+    
+    @IBOutlet var chatListCollectionView: UICollectionView!
+    @IBOutlet var chatListSearchBar: UISearchBar!
+    
+    var chatList = ChatInfo().mockChatList
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationDesign()
+        collectionViewConfig()
     }
-
-
+    
+    func collectionViewConfig() {
+        chatListCollectionView.delegate = self
+        chatListCollectionView.dataSource = self
+        
+        let xib = UINib(nibName: "ChatListCollectionViewCell", bundle: nil)
+        chatListCollectionView.register(xib, forCellWithReuseIdentifier: "ChatListCollectionViewCell")
+    }
+    
+    func navigationDesign() {
+        navigationItem.title = "TRAVEL TALK"
+    }
+    
 }
 
+extension ChatListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return chatList.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = chatListCollectionView.dequeueReusableCell(withReuseIdentifier: "ChatListCollectionViewCell", for: indexPath) as! ChatListCollectionViewCell
+        let row = chatList[indexPath.row]
+        
+        cell.chatRoomImage.image = UIImage(named: row.chatroomImage.first ?? "")
+        cell.chatRoomTitleLabel.text = row.chatroomName
+        cell.chatRoomPreviewLabel.text = row.chatList.last?.message
+        cell.dateLabel.text = row.chatList.last?.date
+        
+        return cell
+    }
+}
