74459a4f85ef51a5b1d192c5920bb76ae3a4fd93
Woohyun Jo <bluewing1004@gmail.com>
2025-01-13 17:17:18 +0900

    [14th/Design] bongflixView로 이동하는 버튼 만들기

---
+++ AutoLayoutProgrammatically/AutoLayoutProgrammatically/App/Bongflix/BongflixViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BongflixViewController.swift
+//  AutoLayoutProgrammatically
+//
+//  Created by 조우현 on 1/13/25.
+//
+
+import UIKit
+
+class BongflixViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- AutoLayoutProgrammatically/AutoLayoutProgrammatically/ViewController.swift
+++ AutoLayoutProgrammatically/AutoLayoutProgrammatically/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    lazy var bongflixButton = {
+        let btn = UIButton()
+        btn.setTitle("BONGFLIX", for: .normal)
+        btn.setTitleColor(.black, for: .normal)
+        btn.layer.cornerRadius = 8
+        btn.layer.borderWidth = 2
+        btn.layer.borderColor = UIColor.red.cgColor
+        btn .addTarget(self, action: #selector(bongflixButtonTapped), for: .touchUpInside)
+        
+        return btn
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        buttonConfig()
     }
-
+    
+    @objc
+    func bongflixButtonTapped() {
+        print(#function)
+        let vc = BongflixViewController()
+        present(vc, animated: true)
+    }
+    
+    func buttonConfig() {
+        view.addSubview(bongflixButton)
+        
+        bongflixButton.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(100)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(68)
+            make.height.equalTo(50)
+        }
+    }
 
 }
 
