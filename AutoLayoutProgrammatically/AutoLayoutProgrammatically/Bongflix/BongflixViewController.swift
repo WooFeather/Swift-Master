dd0efaeeac42c36b25143f1547dc5cb06ad3ce4f
Woohyun Jo <bluewing1004@gmail.com>
2025-01-13 18:08:01 +0900

    [14th/Design] bongflixView 타이틀 label 및 textField 디자인 세팅

--- AutoLayoutProgrammatically/AutoLayoutProgrammatically/Bongflix/BongflixViewController.swift
+++ AutoLayoutProgrammatically/AutoLayoutProgrammatically/Bongflix/BongflixViewController.swift
@@ -6,23 +6,53 @@
 //
 
 import UIKit
+import SnapKit
 
 class BongflixViewController: UIViewController {
+    
+    lazy var titleLabel = {
+        let label = UILabel()
+        label.text = "BONGFLIX"
+        label.textColor = .red
+        label.font = .systemFont(ofSize: 24, weight: .bold)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    lazy var emailTextField: UITextField = configureTextField(placeholder: "이메일 주소 또는 전화번호")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(#function)
+        
+        essentialDesign()
+        labelConfig()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc
+    func textFieldDidEndOnExit() {
+        view.endEditing(true)
     }
-    */
-
+    
+    func configureTextField(placeholder: String) -> UITextField {
+        let textField = UITextField()
+        textField.placeholder = placeholder
+        textField.textAlignment = .center
+        textField.backgroundColor = .lightGray
+        textField.addTarget(self, action: #selector(textFieldDidEndOnExit), for: .editingDidEndOnExit)
+        return textField
+    }
+    
+    func labelConfig() {
+        view.addSubview(titleLabel)
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(44)
+            make.centerX.equalTo(view.snp.centerX)
+            make.height.equalTo(30)
+        }
+    }
+    
+    func essentialDesign() {
+        view.backgroundColor = .black
+    }
 }
