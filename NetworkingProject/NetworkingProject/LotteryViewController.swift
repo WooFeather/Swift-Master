9284e07edb2899c28f37c68522993c983e9534e0
Woohyun Jo <bluewing1004@gmail.com>
2025-01-14 15:37:45 +0900

    [15th/Setting] NetworkingProject 기본세팅

---
+++ NetworkingProject/NetworkingProject/LotteryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LotteryViewController.swift
+//  NetworkingProject
+//
+//  Created by 조우현 on 1/14/25.
+//
+
+import UIKit
+
+class LotteryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
