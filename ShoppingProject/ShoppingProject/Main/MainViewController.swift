e1e60974db48a5cf62950bc91d9d4e263657f8d7
Woohyun Jo <bluewing1004@gmail.com>
2025-01-15 16:47:38 +0900

    [16th/Setting] 프로젝트 기본세팅

---
+++ ShoppingProject/ShoppingProject/Main/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  ShoppingProject
+//
+//  Created by 조우현 on 1/15/25.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
