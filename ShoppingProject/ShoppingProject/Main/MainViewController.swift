4e6a6c8ce0c40c4d032e926699f36b5b4e7c5c6a
Woohyun Jo <bluewing1004@gmail.com>
2025-01-15 17:22:14 +0900

    [16th/Design] MainView 뷰 및 SearchBar 세팅

--- ShoppingProject/ShoppingProject/Main/MainViewController.swift
+++ ShoppingProject/ShoppingProject/Main/MainViewController.swift
@@ -6,15 +6,38 @@
 //
 
 import UIKit
+import Alamofire
+import SnapKit
 
 class MainViewController: UIViewController {
+    
+    let shoppingSearchBar = UISearchBar()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        configureView()
+        configureSearchBar()
     }
-
-
+    
+    func configureView() {
+        view.backgroundColor = .black
+        navigationItem.title = "도봉러의 쇼핑쇼핑"
+        navigationController?.navigationBar.barStyle = .default
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+    }
+    
+    func configureSearchBar() {
+        view.addSubview(shoppingSearchBar)
+        
+        shoppingSearchBar.snp.makeConstraints { make in
+            make.horizontalEdges.top.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(44)
+        }
+        
+        shoppingSearchBar.tintColor = .white
+        shoppingSearchBar.barTintColor = .black
+        shoppingSearchBar.searchTextField.textColor = .white
+        shoppingSearchBar.placeholder = "브랜드, 상품, 프로필, 태그 등"
+    }
 }
-
